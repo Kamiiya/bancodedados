7b0088a0b6769b80de3fada7c68a46dba7533672
Kamiiya <lkamiiya61@gmail.com>
2024-08-30 17:04:12 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,71 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h2>Cadastro de Usuário</h2>
+<?php
+        session_start();
+        $_SESSION ['X']='Seção oficialmente aberta';
+        echo'<hr>';
+        ?>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <form action="processo_cadastro.php" method="post">
-        <label for="nome">Nome</label><br>
-        <input type="text" name="nome" id="name" required> <br><br>
+    <style>
+      .card-login {
+        padding: 30px 0 0 0;
+        width: 350px;
+        margin: 0 auto;
+      }
+    </style>
+  </head>
 
-        <label for="email">Email</label><br>
-        <input type="email" name="email" id="email" required> <br><br>
+  <body>
 
-        <label for="senha">Senha</label><br>
-        <input type="password" name="senha" id="senha" required> <br><br>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+    </nav>
 
-        <input type="submit" value="Cadastro">
+    <div class="container">    
+      <div class="row">
 
-    </form> 
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form action= "newValidaLogin.php" method="post">
+                <div class="form-group">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="form-group">
+                  <input name="senha" type="senha" class="form-control" placeholder="Senha">
+                </div>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                <?php 
+                if(isset ($_GET['login']) && $_GET['login']=='erro') {?>
+                  <div class= "text-danger">
+                    Usuário ou senha invalido(s).
+                  </div>
+                  <?php } ?>
 
-</body>
+                  <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2'){?>
+                    <div class="text-danger">
+                      Faça login para acessar as páginas protegidas.
+                    </div>
+                  <?php } ?>
+
+                  <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
